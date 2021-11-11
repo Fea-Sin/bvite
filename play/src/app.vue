b7e48a158ec2334065457058b24b95859ee44eb2
qunzhongcheng@creditease.cn <qunzhongcheng@creditease.cn>
2021-11-11 16:58:52 +0800

    提交代码

---
+++ play/src/app.vue
@@ -0,0 +1,18 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({});
+</script>
+
+<template>
+  <div>
+    <div class="box">Bvite Vue</div>
+  </div>
+</template>
+
+<style scoped>
+.box {
+  font-size: 20px;
+  color: blueviolet;
+}
+</style>
