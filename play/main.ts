2a746b0bd6382c27fd5e474651cbdaf25e9d0cfd
qunzhongcheng@creditease.cn <qunzhongcheng@creditease.cn>
2021-11-23 09:52:20 +0800

    提交代码

--- play/main.ts
+++ play/main.ts
@@ -3,11 +3,13 @@ import router from "./src/router";
 
 import App from "./src/app.vue";
 import { VfAlert } from "@bvite/components";
+// import BviteUI from "@bvite/bvite-ui";
 
 import "@bvite/theme-chalk/src/index.less";
 
 const app = createApp(App);
 
 app.use(VfAlert);
+// app.use(BviteUI);
 app.use(router);
 app.mount("#play");
