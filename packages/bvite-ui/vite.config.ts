2b75c243abcae760333fa8ec7ba6ba30406b4b9d
qunzhongcheng@creditease.cn <qunzhongcheng@creditease.cn>
2021-12-06 18:39:57 +0800

    提交代码

---
+++ packages/bvite-ui/vite.config.ts
@@ -0,0 +1,22 @@
+import path from "path";
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+export default defineConfig({
+  plugins: [vue()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "index.ts"),
+      name: "VFui",
+      fileName: (format) => `vfui.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["vue"],
+      output: {
+        globals: {
+          vue: "Vue",
+        },
+      },
+    },
+  },
+});
