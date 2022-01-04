fe4fc72b22d888b0e01ada7447a2fac959279048
Fea-Sin <1210992614@qq.com>
2022-01-04 14:19:44 +0800

    chore: chore node version
    chores about node package version

--- packages/utils/props.ts
+++ packages/utils/props.ts
@@ -120,9 +120,13 @@ export function buildProp<
 
           if (values) {
             allowedValues = [...values, defaultValue];
-            valid ||= allowedValues.includes(val);
+            // valid ||= allowedValues.includes(val);
+            if (!valid) valid = allowedValues.includes(val);
           }
-          if (validator) valid ||= validator(val);
+          // if (validator) valid ||= validator(val);
+          if (validator) {
+            if (!valid) valid = validator(val);
+          }
 
           if (!valid && allowedValues.length > 0) {
             const allowValuesText = [...new Set(allowedValues)]
